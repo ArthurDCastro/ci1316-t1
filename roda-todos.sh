e15cfaa9d28ed8e54d2f2385dd78df8569ee2dbc
Eduardo Camargo Neves <56937861+EduCNeves@users.noreply.github.com>
2024-11-04 21:50:07 -0300

    Add files via upload

--- roda-todos.sh
+++ roda-todos.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 echo "USAGE: ./rodaTodos.sh <nElements>"
-    
-# for i in {1..8}
-# do
-#     echo "Executando 10 vezes o bsearch_function_pa com $1 elementos e $i threads:"
-#     for j in {1..10}
-#     do
-#         ./bsearch_function_pa $1 $i | grep -oP '(?<=Tempo total em segundos: )[^ ]*'
-#     done
-# done
 
+echo "Executando bsearch_function_pa"
+    
 for i in {1..8}
 do
-    echo "Executando 10 vezes o bsearch_function_pb com $1 elementos e $i threads:"
+    echo "Executando 10 vezes o bsearch_function_pa com $1 elementos e $i threads:"
     for j in {1..10}
     do
-        ./bsearch_function_pb $1 $i | grep -oP '(?<=Tempo total em segundos: )[^ ]*'
+        ./bsearch_function_pa $1 $i | grep -oP '(?<=total_time_in_seconds: )[^ ]*'
     done
 done
+
+# echo "Executando bsearch_function_pa"
+
+# for i in {1..8}
+# do
+#     echo "Executando 10 vezes o bsearch_function_pb com $1 elementos e $i threads:"
+#     for j in {1..10}
+#     do
+#         ./bsearch_function_pb $1 $i | grep -oP '(?<=total_time_in_seconds: )[^ ]*'
+#     done
+# done
